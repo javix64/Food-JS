05903bd7f7126921f2a2df31535532f7e8042347
javix64 <jav1.x64@gmail.com>
2020-12-24 12:59:13 +0100

    First request in console

---
+++ README.md
@@ -0,0 +1,2 @@
+# Food Facts API
+This API project is for eat better than you are doing right now.
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const url='https://yojavier.ddns.net/walla.json';
-let req= new Request(url);
+var url = 'https://api.edamam.com/search?q=tomato&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068';
+    params = {
+        method: 'GET'
+    };     
+ 
+var request = new Request( url, params );
+ 
+fetch( request )
+  .then( r => r.json() )
+  .then( data => console.dir( data ) )
+  .catch( e => console.error( 'Something went wrong' ) );

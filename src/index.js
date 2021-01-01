0a892a380db9a28a7de21215527eed2567566469
javix64 <jav1.x64@gmail.com>
2021-01-01 18:36:19 +0100

    Finishing the project

--- src/index.js
+++ src/index.js
@@ -1,68 +1,63 @@
-/**
-<article>
-  <h2>Name</h2>
-  <p>Calories</p>
-  <img src="" alt="FOTO"> <br>
-  <ul>
-    <li>ingrediente1</li>
-    <li>ingrediente2</li>
-  </ul>
-</article>
- */
-class Recipe{
-  constructor(url){
-    this.url=url;
-    this.dataJson;
-    this.response;
-    fetch(new Request(this.url,{method: 'GET'}))
-    .then( response => response.json())
-    .then( data => {this.dataJson=data;
-    console.log('datos'+data);
-    console.log(this.dataJson);
-  
-  } )
-    .catch( e => console.error( 'Something went wrong' ) );
-  }
-  getData(){
-    console.log(this.dataJson);
-  }
-  
-}
-const pa= new Recipe('https://api.edamam.com/search?q=potato&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=paleo');
-pa.getData();
-function main(){
-  const article = document.createElement('article');
-  const p = document.createElement('p');
-  const img = document.createElement('img');
-  const ul = document.createElement('ul');
-  const li = document.createElement('li');
+function main(){  
   const urlPaleo = 'https://api.edamam.com/search?q=chicken&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=paleo';
-  const urlVegan = 'https://api.edamam.com/search?q=chicken&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=vegan';
-  const urlLowSugar= 'https://api.edamam.com/search?q=chicken&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=low-sugar';
+  const urlVegan = 'https://api.edamam.com/search?q=broccoli&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=vegan';
+  const urlLowSugar= 'https://api.edamam.com/search?q=tomato&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=low-sugar';
   const arrUrl=[urlPaleo,urlVegan,urlLowSugar];
-  for (let i = 0; i < arrUrl.length; i++) {
-    const requestAll = new Request(arrUrl, params);
+  const arrSection=['paleo','vegan','low-sugar']
+  for (let i = 0; i < 3; i++) {
+    const requestAll = new Request(arrUrl[i],{method: 'GET'});
     fetch(requestAll)
     .then( response => response.json() )
     .then( data => {
-
+      console.dir(data.hits);
+      let hits=data.hits;
+      let article;
+      let p;
+      let a;
+      let img;
+      let ul;
+      let li;
+      let h2;
+      let section = document.getElementById(arrSection[i]);
+      for (let i = 0; i < hits.length; i++) {
+        //creating html
+        article = document.createElement('article');
+        p = document.createElement('p');
+        a= document.createElement('a');
+        img = document.createElement('img');
+        ul = document.createElement('ul');
+        h2 = document.createElement('h2');
+        //appending data to html
+        h2.innerText=hits[i].recipe.label;
+        p.innerText=hits[i].recipe.calories;
+        img.setAttribute('src',hits[i].recipe.image);
+        a.setAttribute('href',hits[i].recipe.url);
+        for (let i = 0; i < hits[i].recipe.ingredients.length; i++) {
+          li = document.createElement('li');
+          li.innerText= hits[i].recipe.ingredients[i].text
+          ul.append(li);
+        }
+        article.append(h2);
+        article.append(p);
+        a.append(img);
+        article.append(a);
+        article.append(ul);
+        //append to correctly section.
+        section.append(article);
+      }//end then call
+    
     })
     .catch( e => console.error( 'Something went wrong' ) );
+    function getData(){
+
+    }
+
   }
-  
+
     
-  /**
-   * <!-- 
-        name -- hits[i].recipe.label
-        calories -- hits[i].recipe.calories
-        ingredientes -- hits[i].recipe.ingredients[i].text
-        image -- hits[i].recipe.image
-        url -- hits[i].recipe.url
-        -->
-   */
+  
 }
-
-
+main();
 function search(param){
   let url = `https://api.edamam.com/search?q=${param}&app_id=cc72277f&app_key=fd7b3902835e935b09f511a64f8c7068&Health=paleo`;
   params = {
@@ -74,12 +69,12 @@ function search(param){
     //this is the promise, if promise is correct, it gives me a response JSON
     .then( response => response.json() )
     // it gives me the data in format JSON
-    .then( data => console.dir( data ) )
+    .then( data => {
+      //tengo que borrar las sections y poner la section de busqueda o 'resultados'
+      console.dir( data.hits );
+    } )
     .catch( e => console.error( 'Something went wrong' ) );
-    
 }//end search
-
-
 
 
 const searchIt=document.getElementsByTagName('input')[0];
